5d4bb40ada1b433f0680922d0ae23c6f59e6957c
Emmy Yardley <emmy@yardley.ca>
2014-02-02 14:46:20 +0000

    Created Post and Comment models with relationships

---
+++ db/migrate/20140202133657_create_comments.rb
@@ -0,0 +1,12 @@
+# comments migration
+
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :post, index: true
+
+      t.timestamps
+    end
+  end
+end
