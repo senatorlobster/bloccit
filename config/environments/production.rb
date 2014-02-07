1ede18d4dbd84379960a02da9936a2c84610ddd7
Emmy Yardley <emmy@yardley.ca>
2014-02-07 14:36:15 -0300

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://lobster-bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { :host => 'lobster-bloccit.herokuapp.com' }  
 end
