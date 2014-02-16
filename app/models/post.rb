d9074d94ebeea8e13d241652f86baa696f111248
Emmy Yardley <emmy@yardley.ca>
2014-02-16 18:13:21 +0000

    Created user profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,7 @@ class Post < ActiveRecord::Base
   mount_uploader :post_image, PostImageUploader
 
   default_scope order('rank DESC')
+  scope :visible_to, lambda { |user| user ? scoped : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -36,7 +36,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200, 200]
+  process :resize_to_fill => [90, 90]
   #
   # def scale(width, height)
   #   # do something
